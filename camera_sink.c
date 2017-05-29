b90fc87df348189a645d009da2211d05e919a81f
zul00 <zul.zulkarnaen@gmail.com>
2017-05-29 17:21:31 +0000

    Impleme loop from glib

--- camera_sink.c
+++ camera_sink.c
@@ -1,28 +1,74 @@
 #include <gst/gst.h>
+#include <glib.h>
 
-int main(int argc, char *argv[]) {
-  GstElement *pipeline, *source, *sink, *caps, *dec;
+/**
+ * @brief Message handler
+ */
+static gboolean bus_call (
+    GstBus     *bus,
+    GstMessage *msg,
+    gpointer    data)
+{
+  GMainLoop *loop = (GMainLoop *) data;
+
+  switch (GST_MESSAGE_TYPE (msg)) {
+
+    case GST_MESSAGE_EOS:
+      g_print ("End of stream\n");
+      g_main_loop_quit (loop);
+      break;
+
+    case GST_MESSAGE_ERROR: {
+      gchar  *debug;
+      GError *error;
+
+      gst_message_parse_error (msg, &error, &debug);
+      g_free (debug);
+
+      g_printerr ("Error: %s\n", error->message);
+      g_error_free (error);
+
+      g_main_loop_quit (loop);
+      break;
+    }
+    default:
+      break;
+  }
+
+  return TRUE;
+}
+
+int main(int argc, char *argv[]) 
+{
+  GMainLoop *loop;
   GstBus *bus;
+  guint bus_watch_id;
+
+  GstElement *pipeline, *source, *sink, *caps, *dec;
   GstMessage *msg;
   GstStateChangeReturn ret;
   GstCaps *filtercaps;
 
-  /* Initialize GStreamer */
-  gst_init (&argc, &argv);
+  /* Initialization */
+  gst_init(&argc, &argv);
+  loop = g_main_loop_new(NULL, FALSE);
 
   /* Create the elements */
-  source = gst_element_factory_make ("v4l2src", "source");
-  caps = gst_element_factory_make("capsfilter", "caps");
-  dec = gst_element_factory_make("jpegdec", "dec");
-  sink = gst_element_factory_make ("filesink", "sink");
-
-  /* Create the empty pipeline */
-  pipeline = gst_pipeline_new ("test-pipeline");
+  pipeline  = gst_pipeline_new("test-pipeline");
+  source    = gst_element_factory_make("v4l2src",     "source");
+  caps      = gst_element_factory_make("capsfilter",  "caps");
+  dec       = gst_element_factory_make("jpegdec",     "dec");
+  sink      = gst_element_factory_make("filesink",    "sink");
 
   if (!pipeline || !source || !sink || !caps || !dec) {
     g_printerr ("Not all elements could be created.\n");
     return -1;
   }
+  
+  /* Add message handler */
+  bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+  bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
+  gst_object_unref (bus);
 
   /* Build the pipeline */
   gst_bin_add_many (GST_BIN (pipeline), source, caps, sink, dec, NULL);
@@ -32,6 +78,7 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  /* Setup pipeline */
   // Source properties
   g_object_set (source, "device", "/dev/video0", NULL);
   
@@ -48,45 +95,19 @@ int main(int argc, char *argv[]) {
   g_object_set(sink, "location", "file2.yuv", NULL);
 
   /* Start playing */
-  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
-  if (ret == GST_STATE_CHANGE_FAILURE) {
-    g_printerr ("Unable to set the pipeline to the playing state.\n");
-    gst_object_unref (pipeline);
-    return -1;
-  }
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
-  /* Wait until error or EOS */
-  bus = gst_element_get_bus (pipeline);
-  msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  g_print ("Running...");
+  g_main_loop_run (loop);
 
-  /* Parse message */
-  if (msg != NULL) {
-    GError *err;
-    gchar *debug_info;
-
-    switch (GST_MESSAGE_TYPE (msg)) {
-      case GST_MESSAGE_ERROR:
-        gst_message_parse_error (msg, &err, &debug_info);
-        g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
-        g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
-        g_clear_error (&err);
-        g_free (debug_info);
-        break;
-      case GST_MESSAGE_EOS:
-        g_print ("End-Of-Stream reached.\n");
-        break;
-      default:
-        /* We should not reach here because we only asked for ERRORs and EOS */
-        g_printerr ("Unexpected message received.\n");
-        break;
-    }
-    gst_message_unref (msg);
-  }
-
-  /* Free resources */
-  gst_object_unref (bus);
+  /* Out of the main loop, clean up nicely */
+  g_print ("Returned\n");
   gst_element_set_state (pipeline, GST_STATE_NULL);
-  gst_object_unref (pipeline);
+
+  g_print ("Deleting pipeline\n");
+  gst_object_unref (GST_OBJECT (pipeline));
+  g_source_remove (bus_watch_id);
+  g_main_loop_unref (loop);
+
   return 0;
 }
-
